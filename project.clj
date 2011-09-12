8cc04844a002a32737c08420c8b5d9d8b2cf4e22
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-09-12 15:48:27 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojure-sqlite-example "0.1.0"
+  :description "A simple example of using SQLite with Clojure"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/java.jdbc "0.0.6"]
+                 [org.xerial/sqlite-jdbc "3.6.13"]])
---
+++ src/clojure_sqlite_example/core.clj
@@ -0,0 +1,36 @@
+(ns clj-sqlite.core
+  (:use [clojure.java.jdbc]))
+
+(def testdata
+  {:date "2011-9-12",
+   :url "http://example.com",
+   :title "SQLite Example",
+   :body "Example using SQLite with Clojure"
+   })
+
+(def db
+  {:classname   "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname     "db/database.db"
+   })
+
+(defn create-db []
+  (try (with-connection db 
+         (create-table :news
+                       [:date :text]
+                       [:url :text]
+                       [:title :text]
+                       [:body :text]))
+       (catch Exception e (println e))))
+
+(create-db)
+
+(with-connection db
+  (insert-records :news testdata))
+
+(def output
+  (with-connection db
+    (with-query-results rs ["select * from news"] (doall rs))))
+
+(keys (first output))
+
